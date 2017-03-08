58fc03bcd02284c83606e8f31b8f43393ac82fa1
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-03-08 16:24:22 -0300

    Desenvolvimento do botão para iniciar a app.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
-var width = window.innerWidth;
-var height = window.innerHeight;
+var width   = window.innerWidth;
+var height  = window.innerHeight;
 
-var intro = document.getElementByClassName("intro")[0];
+var intro       = document.getElementByClassName("intro")[0];
+var historia    = document.getElementByClassName("historia")[0];
+var paragrafos  = document.getElementByClassName("paragrafos")[0];
 
 intro.style.fontSize = width / 30 + "px";
+paragrafos.style.height = height + "px";
 
 window.addEventListener("resize", function() {
     width   = canvas.width  = window.innerWidth;
     height  = canvas.height = window.innerHeight;
     intro.style.fontSize = width / 30 + "px";
+    historia.style.fontSize = width / 20 + "px";
+    paragrafos.style.height = height + "px";
 });
